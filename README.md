1b7d237d3060031e4acf960b1b5be5e9b8b3c5e6
Martin Kleppe <kleppe@ubilabs.net>
2010-06-24 17:07:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# jQuery Touch Events Plugin #
+
+This plugin converts any touch event into desktop-browser-like mouse events. 
+
+Simply include the script into your project to enable drag/drop actions as seen in jQuery UI.
+
+    <script src="jquery.js" type="text/javascript"></script>
+    <script src="jquery-ui.js" type="text/javascript"></script>
+    <script src="jquery.touchevents.js" type="text/javascript"></script>  
+    <script src="your-app.js" type="text/javascript"></script>    
---
+++ jquery.touchevents.js
@@ -0,0 +1,48 @@
+/*
+ * Touch Events - jQuery plugin to simulate mouse events on touch devices.
+ *
+ * By Martin Kleppe <kleppe@ubilabs.net>
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+(function($){
+
+	$(function(){
+		var events = "touchstart touchmove touchend touchcancel";
+		$(document).bind(events, function(){
+		  var type, simulated, touch = event.changedTouches[0];
+
+		  switch (event.type) {
+		    case "touchstart": type = "mousedown"; break;
+		    case "touchmove": type = "mousemove"; break;    
+		    case "touchend": type = "mouseup"; break;
+		    default: return;
+		  }
+
+		  simulated = document.createEvent("MouseEvent");
+		  simulated.initMouseEvent(
+				type, true, true, window, 1,
+	      touch.screenX, touch.screenY,
+	      touch.clientX, touch.clientY, 
+				false, false, false, false, 
+				0, null
+			);
+
+		  touch.target.dispatchEvent(simulated);
+		
+			if (event.type == "touchmove" ){
+			  event.preventDefault();				
+			}
+			
+		}, true);
+	});
+
+})(jQuery);
+
+
+
+
+
